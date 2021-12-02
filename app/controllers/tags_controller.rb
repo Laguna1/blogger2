26d410eb8991ff297c1ce598ef0fe65d23ecb445
Laguna1 <lagunazoomarket@gmail.com>
2021-12-02 02:01:47 +0200

    Display tags list, add more actions for tags controller, add more nav links

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,4 +2,33 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    @tag.save
+    flash.notice = "New tag created"
+    redirect_to tags_path
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    if @tag.destroy
+    flash.notice = "Tag '#{@tag.name}' destroyed!"
+    redirect_to tags_path
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:tag).permit(:name)
+  end
 end
